76e50fbd87a871bfe2cc9f1c5ce77b2b9b358480
brenogcota <brenocota4@gmail.com>
2025-01-15 11:05:13 -0300

    fix some script errors

--- index.js
+++ index.js
@@ -1,47 +1,65 @@
 // banner principal
 
-const slides = document.querySelectorAll('.banner-slide');
-const controls = document.querySelectorAll('.slider-controls button');
-let currentSlide = 0;
-
-function showSlide(index) {
-    slides.forEach((slide, i) => {
-        slide.classList.toggle('active', i === index);
-        controls[i].classList.toggle('active', i === index);
+(() => {
+    const slides = document.querySelectorAll('.banner-slide');
+    const controls = document.querySelectorAll('.slider-controls button');
+    let currentSlide = 0;
+    
+    function showSlide(index) {
+        slides.forEach((slide, i) => {
+            slide.classList.toggle('active', i === index);
+            controls[i].classList.toggle('active', i === index);
+        });
+    }
+    
+    controls.forEach((control, index) => {
+        control.addEventListener('click', () => {
+            currentSlide = index;
+            showSlide(currentSlide);
+        });
     });
-}
-
-controls.forEach((control, index) => {
-    control.addEventListener('click', () => {
-        currentSlide = index;
+    
+    setInterval(() => {
+        currentSlide = (currentSlide + 1) % slides.length;
         showSlide(currentSlide);
-    });
-});
-
-setInterval(() => {
-    currentSlide = (currentSlide + 1) % slides.length;
-    showSlide(currentSlide);
-}, 5000);
+    }, 5000);
+})();
 
 // depoimentos
-const testimonials = document.querySelectorAll('.testimonial');
-let currentIndex = 0;
-
-document.getElementById('next').addEventListener('click', () => {
-    testimonials[currentIndex].classList.remove('active');
-    currentIndex = (currentIndex + 1) % testimonials.length;
-    testimonials[currentIndex].classList.add('active');
-});
-
-
-document.getElementById('prev').addEventListener('click', () => {
-    testimonials[currentIndex].classList.remove('active');
-    currentIndex = (currentIndex - 1 + testimonials.length) % testimonials.length;
-    testimonials[currentIndex].classList.add('active');
+(() => {
+    const testimonials = document.querySelectorAll('.testimonial');
+    let currentIndex = 0;
     
-});
+    // document.getElementById('next').addEventListener('click', () => {
+    //     testimonials[currentIndex].classList.remove('active');
+    //     currentIndex = (currentIndex + 1) % testimonials.length;
+    //     testimonials[currentIndex].classList.add('active');
+    // });
+    
+    
+    // document.getElementById('prev').addEventListener('click', () => {
+    //     testimonials[currentIndex].classList.remove('active');
+    //     currentIndex = (currentIndex - 1 + testimonials.length) % testimonials.length;
+    //     testimonials[currentIndex].classList.add('active');
+    // });
+
+    setInterval(() => {
+        testimonials[currentIndex].classList.remove('active');
+        currentIndex = (currentIndex + 1) % testimonials.length;
+        testimonials[currentIndex].classList.add('active');
+    }, 2000);
+})();
 
 
-
-
+// FAQ
+(() => {
+    document.addEventListener("DOMContentLoaded", () => {
+        const faqs = document.querySelectorAll(".faq h2");
+        faqs.forEach(faq => {
+            faq.addEventListener("click", () => {
+                faq.parentElement.classList.toggle("active");
+            });
+        });
+    });
+})();
 
