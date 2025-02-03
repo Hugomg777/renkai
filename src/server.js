487fa03efbe94573d2647fe4a4f21dd159619c91
brenogcota <brenocota4@gmail.com>
2025-02-03 20:46:59 -0300

    add handlebars

--- src/server.js
+++ src/server.js
@@ -1,32 +1,44 @@
 const express = require("express");
-const path = require("path");
+const exphbs = require("express-handlebars");
 const fs = require("fs");
+const path = require("path");
 
 const app = express();
-const port = 3000;
+const PORT = 3000;
 
-// Define the folder for static files
-app.use(express.static(path.join(__dirname, "views")));
-app.use(express.static(path.join(__dirname, "..", "public"))); // Serve CSS and JS files
-
-// Read files from views folder and create routes dynamically
-const viewsPath = path.join(__dirname, "views");
-fs.readdir(viewsPath, (err, files) => {
-  if (err) {
-    console.error("Error reading views folder:", err);
-    return;
-  }
-
-  files.forEach((file) => {
-    if (file.endsWith(".html")) {
-      const route = file === "index.html" ? "/" : `/${path.parse(file).name}`;
-      app.get(route, (req, res) => {
-        res.sendFile(path.join(viewsPath, file));
-      });
-    }
-  });
+// logger
+app.use((req, res, next) => {
+  const now = new Date().toISOString();
+  console.log(`${req.method} ${req.url} - [${now}]`);
+  next();
 });
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+app.engine(
+    "hbs",
+    exphbs.engine({
+        extname: ".hbs",
+        defaultLayout: "main",
+        layoutsDir: path.join(__dirname, "views/layouts"),
+        partialsDir: path.join(__dirname, "views/partials"),
+    })
+);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "..", "public")));
+
+const viewsPath = path.join(__dirname, "views");
+fs.readdirSync(viewsPath).forEach((file) => {
+    if (file.endsWith(".hbs")) {
+        let routeName = file.replace(".hbs", "");
+        routeName = routeName === 'index' ? '' : routeName;
+        
+        app.get(`/${routeName}`, (req, res) => {
+            res.render(routeName);
+        });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
 });
