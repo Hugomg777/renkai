e776f22ab6d7ab342f7a21d91ab8dbcd24173eac
Hugomg777 <nilsonmeireles008@gmail.com>
2025-02-07 22:15:53 -0300

    subpaginas adicionadas

--- public/index.js
+++ public/index.js
@@ -27,7 +27,7 @@
 
 // depoimentos
 (() => {
-    const array = ["http://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNPzB4HkpYS052y-rij-4_a6DzIiUwWQckww&s","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_JpyvaJSNpF1RWquk6-n4Fk8ag41wNdCHJQ&s","https://asset.japan.travel/image/upload/v1675341685/nagano/Nagano_s_id23_18.jpg","https://apaixonadosporhistoria.com.br/img/foto/galeria_1_751740192.jpg","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsHzIKe_yNRARKFJIJibV5z-P3dJiX8HStdg&s","https://dynamic-media-cdn.tripadvisor.com/media/photo-o/14/b0/69/4f/templo-quan-inn.jpg?w=1200&h=-1&s=1","https://www.viajaquepassa.com.br/wp-content/uploads/templo-branco-chiang-rai-1024x683.jpg","https://offloadmedia.feverup.com/saopaulosecreto.com/wp-content/uploads/2024/05/21145021/2560px-Templo_de_Salomao_-_21.jpg","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOUQPlN3hecutXvPBMujJmUGJpnSpi7c7SVg&s","https://files.mormonsud.net/wp-content/uploads/2022/10/templo-de-belem.jpg","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStRHEKjPoUpOt3Kj8WjNo7KtWLkb46J2gnOw&s"];
+    const array = ["https://images.unsplash.com/photo-1559565779-302744980c7c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D","https://images.unsplash.com/photo-1609949163470-1c83a0aba3fa?q=80&w=2076&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D","https://images.unsplash.com/photo-1616123654498-f864ac762231?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"];
     const testimonials = document.querySelectorAll('.testimonial');
     let currentIndex = 0;
 
@@ -36,7 +36,7 @@
         document.querySelector("#testimonial").style.backgroundImage = "url('"+array[currentIndex]+"')";
         currentIndex = (currentIndex + 1) % testimonials.length;
         testimonials[currentIndex].classList.add('active');
-    }, 2000);
+    }, 6000);
 })();
 
 
